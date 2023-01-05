410514b91582c7a0c7e4cb82f64239cab237b99e
Noah Duran <46541422+noahd320@users.noreply.github.com>
2023-01-05 06:02:12 -0500

    Update dayThreePartOne.py

--- Day 3/dayThreePartOne.py
+++ Day 3/dayThreePartOne.py
@@ -10,15 +10,16 @@ with open("rucksacks.txt") as f:
     line = f.read().strip()
 
 priority = 0
+
 """Set apart rucksacks based on each new line"""
 compartments = line.split("\n")
 for sides in compartments:
     a = len(sides)
-    sideOne = sides[:a//2] #side 1 checking from 0 to half the length
-    sideTwo = sides[a//2:] #side 2 checking from half the length to the end
+    sideOne = sides[:(a//2)] #side 1 checking from 0 to half the length
+    sideTwo = sides[(a//2):] #side 2 checking from half the length to the end
 
-for i, item in enumerate(key):
-    if item in sideOne and item in sideTwo:
-        priority += key.index(item) + 1 #add one on the end for the key starts from 0
+    for i, item in enumerate(key):
+        if item in sideOne and item in sideTwo:
+            priority += key.index(item) + 1 #add one on the end for the key starts from 0
 
 print(priority)
